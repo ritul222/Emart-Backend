5ba0b221af471bb7e133775c2c89ad9bbe440b4f
ritul222 <ritulsinha22@gmail.com>
2025-12-15 15:06:13 +0530

    Admin dashboard fixed

--- src/main/java/com/ecommerce/project/repositories/OrderRepository.java
+++ src/main/java/com/ecommerce/project/repositories/OrderRepository.java
@@ -1,10 +1,15 @@
 package com.ecommerce.project.repositories;
 
-import com.ecommerce.project.model.Order;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import com.ecommerce.project.model.Order;
+
 @Repository
-public interface OrderRepository extends JpaRepository<Order,Long> {
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    @Query("SELECT COALESCE(SUM(o.totalAmount), 0) FROM Order o")
+    Double getTotalRevenue();
 }
